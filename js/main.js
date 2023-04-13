b15613d5fbe1568729b225c89c3572c7413bafa0
Hamidjonova-N <shamsuddinovanigoraxon@gmail.com>
2023-04-13 21:23:40 +0500

    Added dark mode JS codes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let elDarkModeBtn = document.querySelector(".dark-mode-btn");
+let elDarkModeBtnTextWrapper = document.querySelector(".dark-mode-btn-text-wrapper");
+
+elDarkModeBtn.addEventListener("click", function () {
+//   document.body.classList.toggle("dark-mode");
+  if (document.body.classList.toggle("dark-mode") == true) {
+   elDarkModeBtnTextWrapper.textContent = "Light Mode";
+  }
+  else {
+   elDarkModeBtnTextWrapper.textContent = "Dark Mode";
+  }
+});
